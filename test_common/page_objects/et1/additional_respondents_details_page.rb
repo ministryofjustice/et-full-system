772713d3b18963d58f60030de05e911f24dad48a
Gary Taylor <gary.taylor@hismessages.com>
2018-03-21 07:52:44 +0000

    Respondents and additional respondents now working in ET1

---
+++ test_common/page_objects/et1/additional_respondents_details_page.rb
@@ -0,0 +1,67 @@
+require_relative './base_page'
+module EtFullSystem
+  module Test
+    module Et1
+      class AdditionalRespondentsDetailsPage < BasePage
+        section :main_content, '#content .main-section .main-content' do
+          section :more_than_one_employer, :xpath, (XPath.generate { |x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Claims against more than one employer")]] }) do
+            def set(value)
+              choose value, name: "additional_respondents[of_collection_type]"
+            end
+            [2, 3, 4, 5].each_with_index do |number, idx|
+              section :"respondent_#{number}", :xpath, (XPath.generate { |x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Respondent #{idx + 2}")]] }) do |*_args|
+                element :name, "input[name=\"additional_respondents[collection_attributes][#{idx}][name]\"]"
+                element :building, "input[name=\"additional_respondents[collection_attributes][#{idx}][address_building]\"]"
+                element :street, "input[name=\"additional_respondents[collection_attributes][#{idx}][address_street]\"]"
+                element :locality, "input[name=\"additional_respondents[collection_attributes][#{idx}][address_locality]\"]"
+                element :county, "input[name=\"additional_respondents[collection_attributes][#{idx}][address_county]\"]"
+                element :post_code, "input[name=\"additional_respondents[collection_attributes][#{idx}][address_post_code]\"]"
+                element :telephone_number, "input[name=\"additional_respondents[collection_attributes][#{idx}][address_telephone_number]\"]"
+                element :acas_number, "input[name=\"additional_respondents[collection_attributes][#{idx}][acas_early_conciliation_certificate_number]\"]"
+              end
+            end
+            element :add_another_respondent, 'input[value="Add another respondent"]'
+          end
+          element :save_and_continue_button, 'form.edit_additional_respondents input[value="Save and continue"]'
+        end
+
+        def save_and_continue
+          main_content.save_and_continue_button.click
+        end
+
+        def set_for(user)
+          respondents = user[:respondents]
+          return if respondents.nil? || respondents.empty?
+          if respondents.length == 1
+            main_content.more_than_one_employer.set('No')
+          else
+            main_content.more_than_one_employer do |s|
+              s.set('Yes')
+              respondents[1..-1].each_with_index do |respondent, idx|
+                s.add_another_respondent.click unless idx == 0
+                populate_respondent s.send(:"respondent_#{idx + 2}"), respondent
+              end
+            end
+          end
+
+        end
+
+        private
+
+        def populate_respondent(section, respondent)
+          set_field section, :name, respondent
+          set_field section, :building, respondent
+          set_field section, :street, respondent
+          set_field section, :locality, respondent
+          set_field section, :county, respondent
+          set_field section, :post_code, respondent
+          set_field section, :acas_number, respondent
+        end
+
+        def set_field(s, key, data)
+          s.send(key).set(data[key]) if data.key?(key)
+        end
+      end
+    end
+  end
+end
---
+++ test_common/page_objects/et1/respondents_details_page.rb
@@ -0,0 +1,78 @@
+require_relative './base_page'
+module EtFullSystem
+  module Test
+    module Et1
+      class RespondentsDetailsPage < BasePage
+        section :main_content, '#content .main-section .main-content' do
+          section :about_the_respondent, :xpath, (XPath.generate { |x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("About the respondent")]] }) do
+            element :name, 'input[name="respondent[name]"]'
+            element :building, 'input[name="respondent[address_building]"]'
+            element :street, 'input[name="respondent[address_street]"]'
+            element :locality, 'input[name="respondent[address_locality]"]'
+            element :county, 'input[name="respondent[address_county]"]'
+            element :post_code, 'input[name="respondent[address_post_code]"]'
+            element :telephone_number, 'input[name="respondent[address_telephone_number]"]'
+          end
+          section :your_work_address, :xpath, (XPath.generate { |x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Your work address")]] }) do
+            element :building, 'input[name="respondent[work_address_building]"]'
+            element :street, 'input[name="respondent[work_address_street]"]'
+            element :locality, 'input[name="respondent[work_address_locality]"]'
+            element :county, 'input[name="respondent[work_address_county]"]'
+            element :post_code, 'input[name="respondent[work_address_post_code]"]'
+            element :telephone_number, 'input[name="respondent[work_address_telephone_number]"]'
+            section :same_address, '.respondent_worked_at_same_address' do
+              def set(value)
+                choose value, name: 'respondent[worked_at_same_address]'
+              end
+            end
+          end
+          section :acas, :xpath, (XPath.generate { |x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Acas early conciliation certificate number")]] }) do
+            element :certificate_number, 'input[name="respondent[acas_early_conciliation_certificate_number]"]'
+          end
+          element :save_and_continue_button, 'form.edit_respondent input[value="Save and continue"]'
+        end
+
+        def save_and_continue
+          main_content.save_and_continue_button.click
+        end
+
+        def set_for(user)
+          respondents = user[:respondents]
+          return if respondents.nil? || respondents.empty?
+          respondent = respondents.first
+          main_content.about_the_respondent do |s|
+            set_field s, :name, respondent
+            set_field s, :building, respondent
+            set_field s, :street, respondent
+            set_field s, :locality, respondent
+            set_field s, :county, respondent
+            set_field s, :post_code, respondent
+            set_field s, :telephone_number, respondent
+          end
+          if respondent.key?(:work_address)
+            main_content.your_work_address do |s|
+              s.same_address.set('No')
+              set_field s, :building, respondent[:work_address]
+              set_field s, :street, respondent[:work_address]
+              set_field s, :locality, respondent[:work_address]
+              set_field s, :county, respondent[:work_address]
+              set_field s, :post_code, respondent[:work_address]
+              set_field s, :telephone_number, respondent[:work_address]
+            end
+          else
+            main_content.your_work_address.same_address.set('Yes')
+          end
+          main_content.acas do |s|
+            s.certificate_number.set respondent[:acas_number] if respondent.key?(:acas_number)
+          end
+        end
+
+        private
+
+        def set_field(s, key, data)
+          s.send(key).set(data[key]) if data.key?(key)
+        end
+      end
+    end
+  end
+end
