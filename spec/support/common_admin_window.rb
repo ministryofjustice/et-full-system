a2624fc31835198ac86b9e5f317c3fe80517fb55
Gary Taylor <gary.taylor@hismessages.com>
2018-04-13 14:44:04 +0100

    Removed rspec tests

--- spec/support/common_admin_window.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'test_common'
-
-RSpec.configure do |c|
-  c.include EtFullSystem::Test::CommonAdminWindow, type: :feature
-  c.after(:suite) do
-    EtFullSystem::Test::CommonAdminWindow.ensure_admin_window_closed
-  end
-end
