29a236b45fd04a7bec2671143bc66034b063f9aa
Sivvoy Taing <sivvo@yahoo.com>
2018-05-01 17:10:41 +0100

    fix spelling error

--- features/step_definitions/et1_atos_export_steps.rb
+++ features/step_definitions/et1_atos_export_steps.rb
@@ -7,7 +7,7 @@ Then(/^my entered data is formatted into a PDF format file and is available for 
 end
 Then(/^my entered data is formatted into a TXT format file and is available for ATOS to download$/) do
   within_admin_window do
-    dmin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
+    admin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_txt_for, user: user), timeout: 30, sleep: 2
@@ -16,7 +16,7 @@ end
 
 Then(/^my entered data is formatted into an XML format file and is available for ATOS to download$/) do
   within_admin_window do
-    dmin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
+    admin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_xml_for, user: user), timeout: 30, sleep: 2
@@ -25,7 +25,7 @@ end
 
 Then(/^my uploaded ET1 CSV data is formatted into a TXT format file and is available for ATOS to download$/) do
   within_admin_window do
-    dmin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
+    admin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1a_claim_txt_for, user: user), timeout: 30, sleep: 2
@@ -35,7 +35,7 @@ end
 
 Then(/^the CSV file is stored in a landing folder$/) do
   within_admin_window do
-    dmin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
+    admin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_csv_for, user: user), timeout: 30, sleep: 2
@@ -43,7 +43,7 @@ end
 
 Then(/^the RTF file is stored in a landing folder$/) do
   within_admin_window do
-    dmin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
+    admin_pages.claimants_page.check_claimants_details(user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_rtf_for, user: user), timeout: 30, sleep: 2
