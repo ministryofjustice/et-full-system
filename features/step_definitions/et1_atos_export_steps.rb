30de2ad94cd2cba78d1362e897a2dc7b9e4a6306
Sivvoy Taing <sivvo@yahoo.com>
2018-05-09 14:01:23 +0100

    call login within the same method when getting claimants details

--- features/step_definitions/et1_atos_export_steps.rb
+++ features/step_definitions/et1_atos_export_steps.rb
@@ -1,8 +1,7 @@
 Then(/^my entered data is formatted into a PDF format file and is available for ATOS to download$/) do
   within_admin_window do
     api = EtFullSystem::Test::AdminApi.new
-    api.login
-    expect { api.get_claimants }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
+    expect { api.claimants_api }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_pdf_for, user: user), timeout: 30, sleep: 2
@@ -11,8 +10,7 @@ end
 Then(/^my entered data is formatted into a TXT format file and is available for ATOS to download$/) do
   within_admin_window do
     api = EtFullSystem::Test::AdminApi.new
-    api.login
-    expect { api.get_claimants }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
+    expect { api.claimants_api }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_txt_for, user: user), timeout: 30, sleep: 2
@@ -22,8 +20,7 @@ end
 Then(/^my entered data is formatted into an XML format file and is available for ATOS to download$/) do
   within_admin_window do
     api = EtFullSystem::Test::AdminApi.new
-    api.login
-    expect { api.get_claimants }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
+    expect { api.claimants_api }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_xml_for, user: user), timeout: 30, sleep: 2
@@ -32,8 +29,7 @@ end
 Then(/^my uploaded ET1 CSV data is formatted into a TXT format file and is available for ATOS to download$/) do
   within_admin_window do
     api = EtFullSystem::Test::AdminApi.new
-    api.login
-    expect { api.get_claimants }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
+    expect { api.claimants_api }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1a_claim_txt_for, user: user), timeout: 30, sleep: 2
@@ -43,8 +39,7 @@ end
 Then(/^the CSV file is stored in a landing folder$/) do
   within_admin_window do
     api = EtFullSystem::Test::AdminApi.new
-    api.login
-    expect { api.get_claimants }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
+    expect { api.claimants_api }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_csv_for, user: user), timeout: 30, sleep: 2
@@ -53,8 +48,7 @@ end
 Then(/^the RTF file is stored in a landing folder$/) do
   within_admin_window do
     api = EtFullSystem::Test::AdminApi.new
-    api.login
-    expect { api.get_claimants }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
+    expect { api.claimants_api }.to eventually include a_hash_including(first_name: user.dig(:personal, :first_name))
     admin_pages.jobs_page.run_export_claims_cron_job
   end
   expect { atos_interface }.to eventually have_zip_file_containing(:et1_claim_rtf_for, user: user), timeout: 30, sleep: 2
