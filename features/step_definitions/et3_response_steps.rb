0d425a46b1c9328a8591424c901347fb47bbd804
Sivvoy Taing <sivvo@yahoo.com>
2018-05-25 09:09:11 +0100

    remove unused code

--- features/step_definitions/et3_response_steps.rb
+++ features/step_definitions/et3_response_steps.rb
@@ -1,14 +1,3 @@
-# When(/^I submit a completed ET3 form$/) do
-#   et3_helper.answer_all_questions
-#   et3_helper.confirmation_of_supplied_details_page.submit_form
-# end
-
-
-# And(/^I take note of my ET3 reference number$/) do
-#   @my_et3_reference = et3_helper.form_submission_page.reference_number.text
-# end
-
-
 When(/^the completed Employment Tribunal response form is submitted$/) do
   start_a_new_et3_response
   et3_answer_respondents_details
--- features/step_definitions/et3_setup_steps.rb
+++ features/step_definitions/et3_setup_steps.rb
@@ -1,7 +1,3 @@
-# Given(/^I am the respondent "([^"]*)" with a unique first name$/) do |name|
-#   et3_helper.given_i_am(name.to_sym)
-# end
-
 Given(/^an employers response$/) do
   @claimant = FactoryBot.create(:et3_claimant)
   @respondent = FactoryBot.create(:et3_respondent)
