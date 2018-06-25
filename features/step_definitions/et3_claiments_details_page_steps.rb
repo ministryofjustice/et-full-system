24ce49aa03368358d7f0806aa103e0a7fef6a072
Sivvoy Taing <sivvo@yahoo.com>
2018-06-25 15:53:54 +0100

    fix broken test that was caused by refactoring et3 claimaint factory-bot

--- features/step_definitions/et3_claiments_details_page_steps.rb
+++ features/step_definitions/et3_claiments_details_page_steps.rb
@@ -1,6 +1,6 @@
 Given(/^I am on the ET3 claimants details page$$/) do
   @respondent = FactoryBot.create(:et3_respondent)
-  @claimant = FactoryBot.create(:et3_claimant)
+  @claimant = FactoryBot.create_list(:et3_claimant, 1, :agree_with_employment_dates)
   start_a_new_et3_response  
   et3_answer_respondents_details
   expect(claimants_details_page).to be_displayed
