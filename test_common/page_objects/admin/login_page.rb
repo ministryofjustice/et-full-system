ded04f79a3660924464193a9b10df98e0b560adb
Sivvoy Taing <sivvo@yahoo.com>
2018-07-20 20:35:46 +0100

    Update email address file to username

--- test_common/page_objects/admin/login_page.rb
+++ test_common/page_objects/admin/login_page.rb
@@ -3,7 +3,7 @@ module EtFullSystem
     module Admin
       class LoginPage < Admin::BasePage
         set_url "/login"
-        element :email_field, :field, 'Email*'
+        element :email_field, :field, 'Username'
         element :password_field, :field, 'Password*'
         element :login_button, 'input[value=Login]'
 
