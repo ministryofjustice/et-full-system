0a75ebce2e45d084088c0d8c77bd78b253eb2046
Sivvoy Taing <sivvo@yahoo.com>
2018-08-10 09:54:28 +0100

    fixed download pdf url

--- features/step_definitions/et3_landing_page_steps.rb
+++ features/step_definitions/et3_landing_page_steps.rb
@@ -17,6 +17,6 @@ Then(/^I should see other relevant links$/) do
     expect(start_page.sidebar.claim_link['href']).to eq 'https://www.gov.uk/employment-tribunals'
     expect(start_page.sidebar.response_link['href']).to eq 'https://www.gov.uk/being-taken-to-employment-tribunal-by-employee'
     expect(start_page.sidebar.contact_link['href']).to eq 'https://www.gov.uk/guidance/employment-tribunal-offices-and-venues'
-    expect(start_page.sidebar.download_link['href']).to eq 'https://www.employmenttribunals.service.gov.uk/form/pdf/141ET3_0414v2.pdf'
+    expect(start_page.sidebar.download_link['href']).to eq 'https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/719457/et3-eng.pdf'
     expect(start_page.sidebar.more_category_link['href']).to eq 'http://gov.uk/browse/working'
   end
