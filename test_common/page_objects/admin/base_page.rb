c80a2c6808045ea4eebac3560ea3aab849a13f50
Sivvoy Taing <sivvo@yahoo.com>
2018-09-06 15:14:37 +0100

    fixed spelling error on postcodes

--- test_common/page_objects/admin/base_page.rb
+++ test_common/page_objects/admin/base_page.rb
@@ -3,7 +3,7 @@ module EtFullSystem
     module Admin
       class BasePage < ::EtFullSystem::Test::BasePage
         section :menu, '#header ul#tabs' do
-          element :office_postcodes_menu_item, :link, 'Office Post Codes'
+          element :office_postcodes_menu_item, :link, 'Office Postcodes'
           element :acas_menu_item, :link, 'Acas'
           element :acas_certificates_menu_item, :link, 'Certificate Search'
           def choose_acas_certificates
