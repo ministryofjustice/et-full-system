e51c06cd62c877df46b73e6aea49836910034c28
Sivvoy Taing <sivvo@yahoo.com>
2018-09-10 12:02:05 +0100

    parameterise admin username

--- features/end_to_end/admin_permissions.feature
+++ features/end_to_end/admin_permissions.feature
@@ -1,6 +1,6 @@
 @e2e
 @javascript
-Feature: Employment Tribunal 
+Feature: Admin roles and permissions
   As an ET Admin system
   I want to ensure that roles only have the permissions assigned to that user role
 
@@ -9,9 +9,9 @@ Feature: Employment Tribunal
    Then I should have full access to ET admin
 
   Scenario: Super User
-   When a super login
+   When a 'superuser' login
    Then I should have partial access to ET admin
 
-  Scenario: Normal User
-    When a normal user login
+  Scenario: Basic User
+    When a 'user' login
     Then I should have basic access to ET admin
--- features/step_definitions/admin_permissions.rb
+++ features/step_definitions/admin_permissions.rb
@@ -2,30 +2,19 @@ Then(/^I should have full access to ET admin$/) do
   expect(admin_pages.any_page.tab.map {|x| x.text}).to eq(admin_pages.any_page.full_access)
 end
 
-Given("a super login") do
-  admin_username = ::EtFullSystem::Test::Configuration.super_username
-  admin_password = ::EtFullSystem::Test::Configuration.super_password
+When("a {string} login") do |string|
+  admin_username = string
+  admin_password = ::EtFullSystem::Test::Configuration.admin_password
   admin_pages.logout_page.load
   admin_pages.dashboard_page.load
   if admin_pages.login_page.displayed?
-    admin_pages.login_page.login(email: admin_username, password: admin_password)
-    raise "Could not login to admin with username '#{admin_username}' and password '#{admin_password}'" unless admin_pages.dashboard_page.displayed?
+    admin_pages.login_page.login(email: string, password: admin_password)
+    raise "Could not login to admin with username '#{string}' and password '#{admin_password}'" unless admin_pages.dashboard_page.displayed?
   end
 end
 
 Then("I should have partial access to ET admin") do
   expect(admin_pages.any_page.tab.map {|x| x.text}).to eq(admin_pages.any_page.partial_access)
-end
-
-Given("a normal user login") do
-  admin_username = ::EtFullSystem::Test::Configuration.basic_username
-  admin_password = ::EtFullSystem::Test::Configuration.basic_password
-  admin_pages.logout_page.load
-  admin_pages.dashboard_page.load
-  if admin_pages.login_page.displayed?
-    admin_pages.login_page.login(email: admin_username, password: admin_password)
-    raise "Could not login to admin with username '#{admin_username}' and password '#{admin_password}'" unless admin_pages.dashboard_page.displayed?
-  end
 end
 
 Then("I should have basic access to ET admin") do
