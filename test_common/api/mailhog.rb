b67157d0510e4927ece17d6ecec40ef928383adf
Sivvoy Taing <sivvo@yahoo.com>
2018-09-10 07:31:05 +0100

    I have tested this to bits and not sure if its going to work

--- test_common/api/mailhog.rb
+++ test_common/api/mailhog.rb
@@ -3,27 +3,13 @@ require 'httparty'
 module EtFullSystem
   module Test
     class MailApi
-      def claim_submitted(email_address)
-        mail = mailhog_api['items'].select { |a| a['Content']['Headers']['To'] == ["#{email_address}"] && a['Content']['Headers']['Subject'] == ["Employment tribunal: claim submitted"]}[0]
-        mail['Content']['Headers']
-      end
-
-      def claim_started(email_address)
-        mail = mailhog_api['items'].select { |a| a['Content']['Headers']['To'] == ["#{email_address}"] && a['Content']['Headers']['Subject'] == ["Employment tribunal: complete your claim"]}[0]
-        mail['Content']['Headers']
-      end
-
-      def respondent_submitted(email_address)
-        mail = mailhog_api['items'].select { |a| a['Content']['Headers']['To'] == ["#{email_address}"] && a['Content']['Headers']['Subject'] == ["Your Response to Employment Tribunal claim online form receipt"]}[0]
-        mail['Content']['Headers']
+      def mailhog_api
+        delete_mailhog_api
+        response = HTTParty.get(::EtFullSystem::Test::Configuration.mailhog_url)
+        response['items'][0]['Content']['Headers']
       end
 
       private
-
-      def mailhog_api
-        delete_mailhog_api
-        HTTParty.get(::EtFullSystem::Test::Configuration.mailhog_search_url)
-      end
 
       def delete_mailhog_api
         HTTParty.delete(::EtFullSystem::Test::Configuration.mailhog_url)
@@ -31,5 +17,3 @@ module EtFullSystem
     end
   end
 end
-
-
