582e0b9e9007d6dc597bbeb15035ef61dbc92d97
Sivvoy Taing <sivvo@yahoo.com>
2018-10-02 14:27:55 +0100

    filter by entered values and increase timeout

--- test_common/api/admin.rb
+++ test_common/api/admin.rb
@@ -11,7 +11,6 @@ module EtFullSystem
       def get_token
         response = request(:get, url)
         self.csrf_token = response.body.match(/csrf-token" content="([^"]*)"/)[1]
-
       end
 
       def login
@@ -49,10 +48,23 @@ module EtFullSystem
         JSON.parse(acas_logs.body).map(&:with_indifferent_access)
       end
 
-      def admin_diversity_data
+      def admin_diversity_data(data)
         login
-        response = request(:get, "#{url}/diversity_responses.json", cookies: cookies_hash)[0]
-        data = response.delete_if { |k, v| %w"id created_at updated_at".include? k}
+        response = request(:get, "#{url}/diversity_responses.json?
+          claim_type_contains=data[:claim_type]
+          &sex_contains=data[:sex_contains] 
+          &sexual_identity_contains=data[:sexual_identity]
+          &age_group_contains=data[:age_group]
+          &ethnicity_contains=data[:ethnicity]
+          &ethnicity_subgroup_contains=data[:ethnicity_subgroup]
+          &disability_contains=data[:disability]
+          &caring_responsibility_contains=data[:caring_responsibility]
+          &gender_contains=data[:gender]
+          &gender_at_birth_contains=data[:gender_at_birth]
+          &pregnancy_contains=data[:pregnancy]
+          &relationship_contains=data[:relationship]
+          &religion_contains=data[:religion]", cookies: cookies_hash)
+        data = response[0].delete_if { |k, v| %w"id created_at updated_at".include? k}
         return data.inject({}) do |a, (k,v)|
           if v.nil? 
             a[k] = nil
@@ -73,7 +85,7 @@ module EtFullSystem
         cookies_hash.add_cookies(last_response.headers['set-cookie'])
         last_response
       end
-
+      
       attr_accessor :cookies_hash, :last_response, :csrf_token
     end
   end
