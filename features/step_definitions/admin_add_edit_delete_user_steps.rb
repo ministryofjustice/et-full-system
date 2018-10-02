6e69decd866f02ddb8d7434f2cf47009025c9fbd
Sivvoy Taing <sivvo@yahoo.com>
2018-10-02 10:17:40 +0100

    add test to cover bulk users upload

--- features/step_definitions/admin_add_edit_delete_user_steps.rb
+++ features/step_definitions/admin_add_edit_delete_user_steps.rb
@@ -11,29 +11,59 @@ end
 
 Then("they should be able to login") do
   admin_pages.any_page.utility_nav.click_logout
-
-  admin_pages.logout_page.load
-  admin_pages.dashboard_page.load
-  if admin_pages.login_page.displayed?
-    admin_pages.login_page.login(email: @user[:username], password: @user[:password])
-    raise "Could not login to admin with username '#{@user[:username]}' and password '#{@user[:password]}'" unless admin_pages.dashboard_page.displayed?
-  end
+  admin_pages.dashboard_page.admin_login(@user[:username], @user[:password])
 end
 
 Then("have basic access to ET admin system") do
   tab_names = admin_pages.any_page.basic_access
-  expect(admin_pages.any_page.names.map {|x| x.text}).to eq(tab_names)
+  expect(admin_pages.any_page.names.map {|x| x.text}).to match_array(tab_names)
 
   # Tear down
   admin_username = ::EtFullSystem::Test::Configuration.admin_username
   admin_password = ::EtFullSystem::Test::Configuration.admin_password
-  admin_pages.logout_page.load
-  admin_pages.dashboard_page.load
-  if admin_pages.login_page.displayed?
-    admin_pages.login_page.login(email: admin_username, password: admin_password)
-    raise "Could not login to admin with username '#{admin_username}' and password '#{admin_password}'" unless admin_pages.dashboard_page.displayed?
-  end
+  admin_pages.dashboard_page.admin_login(admin_username, admin_password)
   admin_pages.any_page.menu.click_users
-  #Need to search user thats just been and remove them from admin
-  # admin_pages.users_page.assert_users_are_delete
+  admin_pages.users_page.delete_user_from_admin("#{@user[:email]}@blah.com")
+end
+
+Then("have partial access to ET system") do
+  tab_names = admin_pages.any_page.partial_access
+  tab_names[tab_names.index('Logout') -1] =  @user[:name]
+  expect(admin_pages.any_page.names.map {|x| x.text}).to match_array(tab_names)
+
+  # Tear down
+  admin_username = ::EtFullSystem::Test::Configuration.admin_username
+  admin_password = ::EtFullSystem::Test::Configuration.admin_password
+  admin_pages.dashboard_page.admin_login(admin_username, admin_password)
+  admin_pages.any_page.menu.click_users
+  admin_pages.users_page.delete_user_from_admin("#{@user[:email]}@blah.com")
+end
+
+Then("have full access to ET admin system") do
+  tab_names = admin_pages.any_page.full_access  
+  tab_names[tab_names.index('Logout') -1] =  @user[:name]
+  expect(admin_pages.any_page.names.map {|x| x.text}).to match_array(tab_names)
+
+  # Tear down
+  admin_username = ::EtFullSystem::Test::Configuration.admin_username
+  admin_password = ::EtFullSystem::Test::Configuration.admin_password
+  admin_pages.dashboard_page.admin_login(admin_username, admin_password)
+  admin_pages.any_page.menu.click_users
+  admin_pages.users_page.delete_user_from_admin("#{@user[:email]}@blah.com")
+end
+
+Then("import {string} users") do |string|
+  @email = string
+  @users = create(:users)
+  admin_pages.any_page.menu.click_users
+  admin_pages.users_page.click_import_users
+  admin_pages.import_file_page.choose_file(@users)
+end
+
+Then("users have successfully been imported") do
+  admin_pages.users_page.search_by_email('Starts with', @email)
+  admin_pages.users_page.assert_users_are_imported
+
+  admin_pages.any_page.menu.click_users
+  admin_pages.users_page.delete_uploaded_csv_users_from_admin
 end
