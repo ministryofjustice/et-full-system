743017423264032f7e114f0d31728300befd9100
Sivvoy Taing <sivvo@yahoo.com>
2018-10-14 13:54:02 +0100

    add checks to validate claimant details in english

---
+++ features/step_definitions/et1_claimant_details_page_steps.rb
@@ -0,0 +1,34 @@
+Given("a claimant is on Claimant's details page") do
+  @claimants = FactoryBot.create_list(:first_person, 1, :person_data)
+  start_a_new_et1_claim
+  et1_answer_login
+end
+
+Then("Claimant's details page copy texts are displayed in the correct language") do
+  expect(et1_claimant_details_page.has_correct_translation?).to be true
+end
+
+
+# When("user click on Your feedback link") do
+#   et1_apply_page.feedback_notice.feedback_link.click
+# end
+
+# Then("user is taken to the feedback page") do
+#   expect(et1_your_feedback_page.header.text).to be_truthy
+# end
+
+# When("user click on Learn about when you can apply link") do
+#   et1_apply_page.learn_how_to_apply
+# end
+
+# Then("user is taken to the time limits page") do
+#   expect(et1_guide_page.has_correct_translation_for_time_limits?).to be true
+# end
+
+# When("user click on Acas early conciliation certificate number") do
+#   et1_apply_page.acas_certificate_number
+# end
+
+# Then("user is taken to the Acas early conciliation page") do
+#   expect(et1_guide_page.has_correct_translation_for_acas_early_conciliation?).to be true
+# end
--- test_common/page_objects/et1/claimant_details_page.rb
+++ test_common/page_objects/et1/claimant_details_page.rb
@@ -14,88 +14,153 @@ module EtFullSystem
         end
         #page and main header
         section :main_header, '.main-header' do
-          element :page_header, :page_title, 'simple_form.claims.claimant.header', exact: false
+          element :page_header, :page_title, 'claims.claimant.header', exact: false
+        end
+        section :error_message, '#error-summary' do
+          element :error_summary, :content_header, 'simple_form.error_notification.error_summary'
+          element :default_message, :paragraph, 'simple_form.error_notification.default_message'
         end
         section :main_content, '#content .main-section .main-content' do
           #About the claimant
-          element :about_the_claimant_legend, :legend_header, ''
+          element :legend_personal_details, :legend_header, 'claims.claimant.legend_personal_details', exact: false
           #information about the person
-          element :information_about_the_person, :paragraph, ''
+          element :personal_details_description, :paragraph, 'claims.claimant.personal_details_description', exact: false
           #title
-          section :title, :question_labelled, '' do
+          section :title, :question_labelled, 'simple_form.labels.claimant.title' do
+            def set(value)
+              root_element.select(value)
+            end
+            element :error_title, :error, 'activemodel.errors.models.user_session.claimant.attributes.title.inclusion'
+          end
+          #first name
+          section :first_name, :question_labelled, 'simple_form.labels.claimant.first_name' do
+            element :field, :css, "input"
+            delegate :set_field, to: :field
+            element :error_first_name, :error, 'activemodel.errors.models.user_session.claimant.attributes.first_name.inclusion'
+          end
+          #lastname name
+          section :last_name, :question_labelled, 'simple_form.labels.claimant.last_name' do
+            element :field, :css, "input"
+            delegate :set_field, to: :field
+            element :error_last_name, :error, 'activemodel.errors.models.user_session.claimant.attributes.last_name.inclusion'
+          end
+          #Date of birth
+          section :date_of_birth, :legend_header, 'claims.personal_details.date_of_birth', exact: false do
+            element :date_of_birth_hint, :paragraph, 'simple_form.hints.claimant.date_of_birth'
+            section :day, :question_labelled, 'simple_form.labels.claimant.date_of_birth.day' do
+              element :field, :css, "input"
+              delegate :set_field, to: :field
+              element :error_day, :error, 'activemodel.errors.models.user_session.claimant.attributes.date_of_birth.inclusion'
+            end
+            section :month, :question_labelled, 'simple_form.labels..claimant.date_of_birth.month' do
+              element :field, :css, "input"
+              delegate :set_field, to: :field
+              element :error_month, :error, 'errors.messages.blank'
+            end
+            section :year, :question_labelled, 'simple_form.labels..claimant.date_of_birth.year' do
+              element :field, :css, "input"
+              delegate :set_field, to: :field
+              element :error_year, :error, 'errors.messages.blank'
+            end
+          end
+          #Gender
+          section :gender, :question_labelled, 'claims.claimant.gender' do
+            element :male, :form_labelled, 'simple_form.options.claimant.gender.male' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
+            end
+            element :female, :form_labelled, 'simple_form.options.claimant.gender.female' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
+            end
+            element :prefer_not_to_say, :form_labelled, 'simple_form.options.claimant.gender.prefer_not_to_say' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
+            end
+            element :error_gender, :error, 'errors.messages.blank'
+          end
+          #has special needs
+          section :claiman_has_special_needs, '.form-group-reveal' do
+            element :has_special_needs, :form_labelled, 'simple_form.labels.claimant.has_special_needs', exact: false
+            element :has_special_needs_hint, :paragraph, 'simple_form.hints.claimant.has_special_needs', exact: false
+            element :yes, :form_labelled, 'simple_form.yes' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
+            end
+            element :no, :form_labelled, 'simple_form.no' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
+            end
+            element :error_has_special_needs, :error, 'errors.messages.blank'
+          end
+          #describe the assistance you require
+          element :special_needs, :question_labelled, 'simple_form.hints.labels.claimant.special_needs' do
+            element :field, :css, "textarea"
+            delegate :set_field, to: :field
+          end
+          #Claimant's contact details
+          element :claimants_contact_details, :legend_header, 'claims.claimant.legend_contact_details', exact: false
+          element :building, :question_labelled, 'simple_form.labels.claimant.address_building' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
+            element :error_building, :error, 'errors.messages.blank'
+          end
+          element :street, :question_labelled, 'simple_form.labels.claimant.address_street' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
+            element :error_street, :error, 'errors.messages.blank'
+          end
+          element :locality, :question_labelled, 'simple_form.labels.claimant.address_locality' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
+            element :error_locality, :error, 'errors.messages.blank'
+          end
+          #County
+          element :county, :question_labelled, 'simple_form.labels.claimant.address_county' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
+            element :error_county, :error, 'errors.messages.blank'
+          end
+          element :county_hint, :paragraph, 'simple_form.hints.claimant.address_county', exact: false
+          element :post_code, :question_labelled, 'simple_form.labels.claimant.address_post_code' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
+            element :error_post_code, :error, 'errors.messages.blank'
+          end
+          section :country, :question_labelled, 'simple_form.labels.claimant.address_country' do
             def set(value)
               root_element.select(value)
             end
           end
-          #first name
-          section :first_name, :question_labelled, '' do
-            element :field, :css, "input"
-            delegate :set, to: :field
+          element :telephone_number, :question_labelled, 'simple_form.labels.claimant.address_telephone_number' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
           end
-          #lastname name
-          section :last_name, :question_labelled, '' do
-            element :field, :css, "input"
-            delegate :set, to: :field
+          element :alternative_telephone_number, :question_labelled, 'simple_form.labels.claimant.mobile_number' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
           end
-          #Date of birth
-          section :date_of_birth_section, 'fieldset' do
-            section :date_of_birth, :form_labelled, '' do
-              section :day, :question_labelled, '' do
-                element :field, :css, "input"
-                delegate :set, to: :field
-              end
-              section :month, :question_labelled, '' do
-                element :field, :css, "input"
-                delegate :set, to: :field
-              end
-              section :year, :question_labelled, '' do
-                element :field, :css, "input"
-                delegate :set, to: :field
-              end
+          #TODO ----------
+          element :email_address, :question_labelled, 'simple_form.labels.claimant.email_address' do
+            element :field, :css, 'input'
+            delegate :set_field, to: :field
+            element :error_email_address, :error, 'errors.messages.blank'
+          end
+          #correspondence
+          section :claimant_contact_preference, '.claimant_contact_preference' do
+            element :correspondence, :form_labelled, 'simple_form.labels.claimant.contact_preference'
+            element :contact_preference, :paragraph, 'simple_form.hints.claimant.contact_preference'
+            section :email_preference, :form_labelled, 'simple_form.options.claimant.contact_preference.email' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
             end
-          end
-          #Gender
-          section :gender, :form_labelled, '' do
-            element :male, :question_labelled, ''
-            element :female, :question_labelled, ''
-            element :prefer_not_to_say, :question_labelled, ''
-            def set(value)
-              choose(value, name: 'claimant[gender]')
-            end
-          end
-          #disability
-          section :has_special_needs, :question_labelled, '' do
-            element :yes, :question_labelled, ''
-            element :no, :question_labelled, ''
-            def set(value)
-              choose(value, name: 'claimant[has_special_needs]')
-            end
-          end
-          element :special_needs, :question_labelled, ''
-          #Claimant's contact details
-          section :claimants_contact_details, :question_labelled, ''  do
-            element :building, :question_labelled, ''
-            element :street, :question_labelled, ''
-            element :locality, :question_labelled, ''
-            element :county, :question_labelled, ''
-            element :post_code, :question_labelled, ''
-            section :country, :question_labelled, '' do
-              def set(value)
-                root_element.select(value)
-              end
-            end
-            element :telephone_number, :question_labelled, ''
-            element :alternative_telephone_number, :question_labelled, ''
-            element :email_address, :question_labelled, ''
-            #best way to send correspondence
-            section :correspondence, :question_labelled, '' do
-              def set(value)
-                choose(value, name: 'claimant[contact_preference]')
-              end
+            section :post_preference, :form_labelled, 'simple_form.options.claimant.contact_preference.post' do 
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set_field, to: :selector
             end
           end
           #Save and continue
-          element :save_and_continue_button, :question_labelled, ''
+          element :save_and_continue_button, :submit_text, 'helpers.submit.update'
         end
         #Support links
         section :support, 'aside[role="complementary"]' do
@@ -104,6 +169,58 @@ module EtFullSystem
           element :contact_use, :link_named, 'shared.aside.contact_us'
           element :your_claim, :support_header, 'shared.aside.actions_header'
           element :save_and_complete_later, :link_named, 'shared.mobile_nav.save_and_complete'
+        end
+        def has_correct_translation?
+          #your feedback header
+          expect(feedback_notice.language.text).to be_truthy
+          expect(feedback_notice.feedback_link.text).to be_truthy
+          #Page header
+          expect(main_header.page_header.text).to be_truthy
+          #About your claimant
+          expect(main_content.legend_personal_details.text).to be_truthy
+          expect(main_content.personal_details_description.text).to be_truthy
+          expect(main_content.title.text).to be_truthy
+          expect(main_content.first_name.text).to be_truthy
+          expect(main_content.last_name.text).to be_truthy
+          #date of birth
+          expect(main_content.date_of_birth.text).to be_truthy
+          expect(main_content.date_of_birth.date_of_birth_hint.text).to be_truthy
+          expect(main_content.date_of_birth.day.text).to be_truthy
+          expect(main_content.date_of_birth.month.text).to be_truthy
+          expect(main_content.date_of_birth.year.text).to be_truthy
+          #gender
+          expect(main_content.gender.text).to be_truthy
+          expect(main_content.gender.male.text).to be_truthy
+          expect(main_content.gender.female.text).to be_truthy
+          expect(main_content.gender.prefer_not_to_say.text).to be_truthy
+          #has special needs
+          expect(main_content.claiman_has_special_needs.has_special_needs.text).to be_truthy
+          expect(main_content.claiman_has_special_needs.has_special_needs_hint.text).to be_truthy
+          expect(main_content.claiman_has_special_needs.yes.text).to be_truthy
+          expect(main_content.claiman_has_special_needs.no.text).to be_truthy
+          #Claimant's contact details
+          expect(main_content.claimants_contact_details.text).to be_truthy
+          expect(main_content.building.text).to be_truthy
+          expect(main_content.street.text).to be_truthy
+          expect(main_content.locality.text).to be_truthy
+          expect(main_content.county.text).to be_truthy
+          expect(main_content.county_hint.text).to be_truthy
+          expect(main_content.post_code.text).to be_truthy
+          expect(main_content.country.text).to be_truthy
+          expect(main_content.telephone_number.text).to be_truthy
+          expect(main_content.alternative_telephone_number.text).to be_truthy
+          expect(main_content.email_address.text).to be_truthy
+          #Best way to send correspondence
+          expect(main_content.claimant_contact_preference.correspondence.text).to be_truthy
+          expect(main_content.claimant_contact_preference.contact_preference.text).to be_truthy
+          expect(main_content.claimant_contact_preference.email_preference.text).to be_truthy
+          expect(main_content.claimant_contact_preference.post_preference.text).to be_truthy
+          #Save and continue
+          expect(save_and_continue.text).to be_truthy
+          #Support
+          expect(support.suport_header.text).to be_truthy
+          expect(support.guide.text).to be_truthy
+          expect(support.contact_use.text).to be_truthy
         end
         # Fills in the entire page for the user given
         #
