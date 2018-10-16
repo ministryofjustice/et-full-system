db60d566cd76c067e5820b367c908c304c3d2dea
Sivvoy Taing <sivvo@yahoo.com>
2018-10-16 10:40:59 +0100

    removed unused code

--- features/step_definitions/et1_claimant_details_page_steps.rb
+++ features/step_definitions/et1_claimant_details_page_steps.rb
@@ -23,28 +23,3 @@ end
 Then("I should see the option to describe the assistant I need") do
   expect(et1_claimant_details_page.has_correct_translation_for_assistance_required?).to be true
 end
-
-
-# When("user click on Your feedback link") do
-#   et1_apply_page.feedback_notice.feedback_link.click
-# end
-
-# Then("user is taken to the feedback page") do
-#   expect(et1_your_feedback_page.header.text).to be_truthy
-# end
-
-# When("user click on Learn about when you can apply link") do
-#   et1_apply_page.learn_how_to_apply
-# end
-
-# Then("user is taken to the time limits page") do
-#   expect(et1_guide_page.has_correct_translation_for_time_limits?).to be true
-# end
-
-# When("user click on Acas early conciliation certificate number") do
-#   et1_apply_page.acas_certificate_number
-# end
-
-# Then("user is taken to the Acas early conciliation page") do
-#   expect(et1_guide_page.has_correct_translation_for_acas_early_conciliation?).to be true
-# end
