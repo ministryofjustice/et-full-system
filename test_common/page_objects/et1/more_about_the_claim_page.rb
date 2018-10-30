2b3442bbf9d7ada517ecae9f43346b16c5b51da9
Sivvoy Taing <sivvo@yahoo.com>
2018-10-30 22:03:08 +1100

    add translation for more about the claim page

--- test_common/page_objects/et1/more_about_the_claim_page.rb
+++ test_common/page_objects/et1/more_about_the_claim_page.rb
@@ -17,13 +17,32 @@ module EtFullSystem
           element :page_header, :page_title, 'claims.additional_information.header', exact: false
         end
         section :main_content, '#content .main-section .main-content' do
-          section :other_important_details, :xpath, (XPath.generate { |x| x.descendant(:fieldset)[x.descendant(:legend)[x.string.n.is("Other important details")]] }) do
-            element :notes, 'textarea[name="additional_information[miscellaneous_information]"]'
+          #Other important details
+          section :other_important_details, :legend_header, 'claims.additional_information.legend' do
+            #Do you want to provide additional information about your claim? (optional)
+            element :miscellaneous_information, :form_labelled, 'claims.additional_information.has_miscellaneous_information'
+            #Include anything that will help the tribunal come to a decision about whether your case can be heard. What you write may be seen by the respondent
+            element :miscellaneous_information_hint , :form_hint, 'claims.additional_information.has_miscellaneous_information_hint'
+            include ::EtFullSystem::Test::I18n
+            element :yes, :form_labelled, 'claims.additional_information.yes' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set, to: :selector
+            end
+            element :no, :form_labelled, 'claims.additional_information.no' do
+              element :selector, :css, 'input[type="radio"]'
+              delegate :set, to: :selector
+            end
             def set(value)
-              choose value, name: "additional_information[has_miscellaneous_information]"
+              choose(factory_translate(value), name: 'additional_information[has_miscellaneous_information]')
             end
           end
-
+          
+          section :additional_information_miscellaneous_information, '.additional_information_miscellaneous_information' do
+            #Enter more detail about your claim. Limit is 2500 characters. (2500 characters remaining)
+            element :additonal_miscellaneous_information_hint, :form_hint, 'simple_form.hints.additional_information.miscellaneous_information', exact: false
+            element :field, :css, 'textarea'
+            delegate :set, to: :field
+          end
           #Save and continue
           element :save_and_continue_button, :submit_text, 'helpers.submit.update', exact: false
         end
@@ -40,21 +59,41 @@ module EtFullSystem
           main_content.save_and_continue_button.click
         end
 
+        def has_correct_translation?
+          #your feedback header
+          expect(feedback_notice).to have_language
+          expect(feedback_notice).to have_feedback_link
+          #More about the claim
+          expect(main_header).to have_page_header
+          #Other important details
+          expect(main_content).to have_other_important_details
+          expect(main_content.other_important_details).to have_miscellaneous_information
+          expect(main_content.other_important_details).to have_miscellaneous_information_hint
+          expect(main_content.other_important_details).to have_yes
+          expect(main_content.other_important_details).to have_no
+          #Enter more details about your claim
+          expect(main_content.additional_information_miscellaneous_information).to have_additonal_miscellaneous_information_hint
+          #save and continue
+          expect(main_content).to have_save_and_continue_button
+          #Support links
+          expect(support).to have_suport_header
+          expect(support).to have_guide
+          expect(support).to have_contact_use
+          #Save your claim later
+          expect(support).to have_your_claim
+          #TODO this has stopped working - why?
+          # expect(support).to have_save_and_complete_later
+        end
+
         def set(claim)
           data = claim.to_h
           return if data.nil?
           if data.key?(:other_important_details)
-            main_content.other_important_details.set 'Yes'
-            main_content.other_important_details.notes.set data[:other_important_details]
+            main_content.other_important_details.set(:'claims.additional_information.yes')
+            main_content.additional_information_miscellaneous_information.set data[:other_important_details]
           else
-            main_content.other_important_details.set 'No'
+            main_content.other_important_details.set(:'claims.additional_information.no')
           end
-        end
-
-        private
-
-        def set_field(s, key, data)
-          s.send(key).set(data[key]) if data.key?(key)
         end
       end
     end
--- test_common/page_objects/et1/submission_page.rb
+++ test_common/page_objects/et1/submission_page.rb
@@ -3,6 +3,19 @@ module EtFullSystem
   module Test
     module Et1
       class SubmissionPage < BasePage
+        include RSpec::Matchers
+        #your feedback header
+        section :feedback_notice, '.feedback-notice' do
+          include ::EtFullSystem::Test::I18n
+          element :language, :link_named, 'switch.language'
+          element :welsh_link, :link_or_button, t('switch.language', locale: :en)
+          element :english_link, :link_or_button, t('switch.language', locale: :cy)
+          element :feedback_link, :paragraph, 'shared.feedback_link.feedback_statement_html'
+        end
+        #Check your claim
+        section :main_header, '.main-header' do
+          element :page_header, :main_header, 'claim_reviews.show.header', exact: false
+        end
         section :main_content, '#content .main-section .main-content' do
           element :submit_claim_button, 'form.new_confirmation_email input[value="Submit claim"]'
         end
