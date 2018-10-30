ea5fde245eb0755665ec361bf077ad26cd9f7fe3
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-10-30 10:18:56 +0000

    Add page object for Confirmation Page

--- test_common/page_objects/diversity/confirmation_page.rb
+++ test_common/page_objects/diversity/confirmation_page.rb
@@ -3,14 +3,33 @@ module EtFullSystem
   module Test
     module Diversity
       class ConfirmationPage < BasePage
-        section :main_content, '#content .container' do
-          element :main_header, '.main-header h1 span'
-          element :confirmation, '#edit_diversities_confirmation .form-fields div p'
-          element :return_button, 'a[title="return to GOV.UK website"]'
+        include RSpec::Matchers
+        set_url "#{::EtFullSystem::Test::Configuration.diversity_url}/confirmation"
+
+        section :feedback_notice, '.feedback-notice' do
+          include ::EtFullSystem::Test::I18n
+          element :language, :link_named, 'switch.language'
+          element :welsh_link, :link_or_button, t('switch.language', locale: :en)
+          element :english_link, :link_or_button, t('switch.language', locale: :cy)
+        end
+
+        section :main_content, '.container' do
+
+          element :header, :main_header, 'confirmation.header'
+          element :content, :paragraph, 'confirmation.content'
+          element :return_button, :link_named, 'confirmation.button_text'
         end
 
         def return_to_gov_uk
           main_content.return_button.click
+        end
+
+        def has_correct_translation?
+          expect(feedback_notice).to have_language
+
+          expect(main_content).to have_header
+          expect(main_content).to have_content
+          expect(main_content).to have_return_button
         end
       end
     end
