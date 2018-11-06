0d5aace24071c90e9666d009069cf25033de2f0f
Gary Taylor <gary.taylor@hismessages.com>
2018-11-06 18:33:34 +0000

    Tidy up

--- test_common/matchers/match_et1_pdf_for.rb
+++ test_common/matchers/match_et1_pdf_for.rb
@@ -11,22 +11,17 @@ module EtFullSystem
         self.representative = representative
         self.respondents = respondents
         self.employment = employment
-        self.mismatches = []
       end
 
       # @param [File] actual The pdf file to test
       def matches?(actual)
         self.page_object = EtFullSystem::Test::FileObjects::Et1PdfFile.new(actual)
-        page_object.has_correct_contents_for?(claim: claim, claimants: claimants, respondents: respondents, representative: representative, employment: employment, errors: mismatches, indent: 1)
-      end
-
-      def failure_message
-        "The data provided did not match the ET1 PDF - here are the details: \n\n#{mismatches.join("\n")}"
+        page_object.has_correct_contents_for?(claim: claim, claimants: claimants, respondents: respondents, representative: representative, employment: employment)
       end
 
       private
 
-      attr_accessor :mismatches, :page_object, :claimants, :respondents, :representative, :employment, :claim
+      attr_accessor :page_object, :claimants, :respondents, :representative, :employment, :claim
     end
   end
 end
