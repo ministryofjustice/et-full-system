8f121a641cd732acff0b99c498ff49cf0d99aaa1
Gary Taylor <gary.taylor@hismessages.com>
2018-11-06 13:28:30 +0000

    Fix to employment details section in pdf tests

--- test_common/file_objects/et1_pdf_file/employment_details_section.rb
+++ test_common/file_objects/et1_pdf_file/employment_details_section.rb
@@ -5,16 +5,31 @@ module EtFullSystem
       module Et1PdfFileSection
         class EmploymentDetailsSection < EtFullSystem::Test::FileObjects::Et1PdfFileSection::Base
           def has_contents_for?(employment:, errors:, indent:)
-            validate_fields section: :employment_details, errors: errors, indent: indent do
-              expected_values = {
-                  job_title: employment.try(:job_title),
-                  start_date: employment.try(:start_date),
-                  employment_continuing: yes_no_for(employment.try(:end_date).nil? || date_in_future(employment.try(:end_date), optional: true).present?).downcase,
-                  ended_date: date_in_past(employment.try(:end_date), optional: true) || '',
-                  ending_date: date_in_future(employment.try(:end_date), optional: true) || ''
-              }
-              expect(mapped_field_values).to include expected_values
-            end
+            employment.nil? ? has_contents_for_no_employment? : has_contents_for_employment?(employment)
+          end
+
+          private
+
+          def has_contents_for_employment(employment)
+            expected_values = {
+                job_title: employment.job_title,
+                start_date: employment.start_date,
+                employment_continuing: yes_no_for(employment.end_date.nil? || date_in_future(employment.end_date).present?).downcase,
+                ended_date: date_in_past(employment.try(:end_date), optional: true) || '',
+                ending_date: date_in_future(employment.try(:end_date), optional: true) || ''
+            }
+            expect(mapped_field_values).to include expected_values
+          end
+
+          def has_contents_for_no_employment?
+            expected_values = {
+                job_title: nil,
+                start_date: nil,
+                employment_continuing: nil,
+                ended_date: nil,
+                ending_date: nil
+            }
+            expect(mapped_field_values).to include expected_values
           end
         end
       end
