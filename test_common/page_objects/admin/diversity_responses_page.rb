7c94e080af63be1f0268f94b5763e37c8d37fb64
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-11-16 08:29:21 +0000

    RST-1506: Fix flaky Diversity Religion test (#109)

--- test_common/page_objects/admin/diversity_responses_page.rb
+++ test_common/page_objects/admin/diversity_responses_page.rb
@@ -10,7 +10,7 @@ module EtFullSystem
             acc[k] = factory_translate(v)
             acc
           end
-          expect {api.admin_diversity_data.symbolize_keys}.to eventually include(expected_data)
+          expect {api.admin_diversity_data.symbolize_keys}.to eventually include(expected_data), timeout: 30, sleep: 2
           true
         end
       end
