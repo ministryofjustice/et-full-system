a1a299bbd780adb51add188522a58980bfb69b73
garytaylor <gary.taylor@hismessages.com>
2018-11-20 14:42:40 +0000

    Hotfix/screenshot s3 (#111)
    * Removed package.json and lock file (no longer needed)
    Configured capybara screenshot to use S3
    
    * Updated rubyzip due to security issue
    Updated selenium-webdriver due to security issue in ffi
    
    * Updated rubyzip due to security issue
    Updated selenium-webdriver due to security issue in ffi

--- features/support/capybara_screenshot.rb
+++ features/support/capybara_screenshot.rb
@@ -1,6 +1,31 @@
 require_relative './test_common'
 require 'capybara-screenshot/cucumber'
 
+module Capybara
+  module Screenshot
+    class S3Saver
+      def output_screenshot_path
+        if html_saved?
+          unsigned_url = html_path
+          key = unsigned_url.split('/').last
+          signer = Aws::S3::Presigner.new client: s3_client
+          url = signer.presigned_url(:get_object, bucket: bucket_name, key: key, expires_in: 14400)
+          output "HTML screenshot: #{url}"
+        end
+
+        if screenshot_saved?
+          unsigned_url = screenshot_path
+          key = unsigned_url.split('/').last
+          signer = Aws::S3::Presigner.new client: s3_client
+          url = signer.presigned_url(:get_object, bucket: bucket_name, key: key, expires_in: 14400)
+          output "Image screenshot: #{url}"
+        end
+      end
+    end
+  end
+end
+
+
 After do |scenario|
   if Capybara::Screenshot.autosave_on_failure && scenario.failed?
     Capybara.using_session(Capybara::Screenshot.final_session_name) do
--- test_common/capybara/capybara_screenshot.rb
+++ test_common/capybara/capybara_screenshot.rb
@@ -39,3 +39,15 @@ Capybara::Screenshot.register_driver(:firefoxdriver) do |driver, path|
     driver.browser.save_screenshot(path)
   end
 end
+if ENV.key?('SCREENSHOT_S3_ACCESS_KEY_ID')
+  Capybara::Screenshot.prune_strategy = { keep: 20 }
+  Capybara::Screenshot.s3_configuration = {
+      s3_client_credentials: {
+          access_key_id: ENV.fetch('SCREENSHOT_S3_ACCESS_KEY_ID'),
+          secret_access_key: ENV.fetch('SCREENSHOT_S3_SECRET_ACCESS_KEY'),
+          region: ENV.fetch('SCREENSHOT_S3_REGION'),
+      },
+      bucket_name: ENV.fetch('SCREENSHOT_S3_BUCKET'),
+      key_prefix: ENV.fetch('SCREENSHOT_S3_KEY_PREFIX', '')
+  }
+end
