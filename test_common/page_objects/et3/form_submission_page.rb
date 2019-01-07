6b5161934b5cc586f001a36527bd34ab7c22d256
Sivvoy Taing <sivvo@yahoo.com>
2019-01-07 19:14:10 +1100

    revert pdf ccs fix as it will break system/et3 develop branch

--- test_common/page_objects/et3/form_submission_page.rb
+++ test_common/page_objects/et3/form_submission_page.rb
@@ -10,7 +10,7 @@ module EtFullSystem
         element :submission_confirmation, :css, '.submission-confirmation'
         element :reference_number, :css, '.reference-number'
         element :submission_date, :css, '.submission-date'
-        element :download_pdf, :css, '.pdf-success'
+        element :download_pdf, :css, '.download-pdf'
         element :return_to_govuk_button, :css, 'a.button.button-start'
         def return
           return_to_govuk_button.click
