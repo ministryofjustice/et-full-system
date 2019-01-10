0e8b5fcf56cd7bfd1635e2b361315973290a5d84
Sivvoy Taing <sivvo@yahoo.com>
2019-01-10 23:53:05 +1100

    replaced response complete in welsh email

--- features/support/email_support/email_objects/et3_response_email_html.rb
+++ features/support/email_support/email_objects/et3_response_email_html.rb
@@ -1,61 +1,99 @@
 require 'mail'
 require_relative './base'
+require_relative './office_helper'
 require 'rack/utils'
 module EtFullSystem
   module Test
     class Et3ResponseEmailHtml < SitePrism::Page
       include RSpec::Matchers
-      element(:reference_element, :xpath, XPath.generate { |x| x.descendant(:p)[x.string.n.starts_with('This is your reference number:')] })
-      element(:submission_date_element, :xpath, XPath.generate { |x| x.descendant(:p)[x.string.n.starts_with('Submission date:')] })
-      element(:office_address_element, :xpath, XPath.generate { |x| x.descendant(:p)[x.string.n.starts_with('Office address:')] })
-      element(:office_telephone_element, :xpath, XPath.generate { |x| x.descendant(:p)[x.string.n.starts_with('Telephone:')] })
-      element(:office_name_element, :xpath, XPath.generate { |x| x.descendant(:p)[x.string.n.starts_with('Thank you for your submission. It has been forwarded to the')] })
+      include EtFullSystem::Test::OfficeHelper
+      include EtFullSystem::Test::I18n
 
-      def self.find(search_url: ::EtFullSystem::Test::Configuration.mailhog_search_url, reference:, sleep: 30, timeout: 120)
-        item = find_email(reference, search_url, sleep: sleep, timeout: timeout)
-        raise "ET3 Mail with reference #{reference} not found" unless item.present?
-        new(item)
+      def self.find(search_url: ::EtFullSystem::Test::Configuration.mailhog_search_url, reference:, locale:, sleep: 10, timeout: 120)
+        instances = search_url.map { |mail| Et3ResponseEmailHtml.new(mail, locale: locale) }
+        instances.detect { |instance| oinstance.has_correct_subject? && instance.has_reference_element?(reference) }
       end
 
-      def self.find_email(reference, search_url, timeout: 120, sleep: 10)
-        Timeout.timeout(timeout) do
-          item = nil
-          until item.present? do
-            query = Rack::Utils.build_query(kind: 'containing', query: reference, start: 0, limit: 1)
-            url = URI.parse(search_url)
-            url.query = query
-            response = HTTParty.get(url, headers: { accept: 'application/json' })
-            item = response.parsed_response['items'].first
-            sleep sleep unless item.present?
-          end
-          Mail.new item.dig('Raw', 'Data')
-        end
-      rescue Timeout::Error
-        return nil
-      end
-
-      def initialize(mail)
+      def initialize(mail, locale:)
         self.mail = mail
+        self.locale = locale
         multipart = mail.parts.detect { |p| p.content_type =~ %r{multipart\/alternative} }
         part = multipart.parts.detect { |p| p.content_type =~ %r{text\/html} }
         body = part.nil? ? '' : part.body.to_s
         load(body)
       end
 
-      # The reference number coming from inside the email
-      def reference
-        reference_element.text.split(':').last.strip.tr(' which should be quoted on all correspondence.', '')
+      def has_reference_element?(reference)
+        assert_reference_element(reference)
+        true
+      rescue Capybara::ElementNotFound
+        false
       end
 
-      def submission_date
-        submission_date_element.text.gsub(/\A.*?:/, '').strip
+      def has_correct_content_for?(input_data, reference:) # rubocop:disable Naming/PredicateName
+        office = office_for(case_number: input_data.case_number)
+        aggregate_failures 'validating content' do
+          assert_reference_element(reference)
+          expect(has_correct_subject?).to be true
+          expect(has_correct_to_address_for?(input_data)).to be true
+          assert_office_name_element(office.name)
+          assert_office_address_element(office.address)
+          assert_office_telephone_element(office.telephone)
+          assert_submission_date
+          expect(attached_pdf_for(reference: reference)).to be_present
+        end
+        true
       end
 
       def has_correct_subject? # rubocop:disable Naming/PredicateName
-        mail.subject == 'Your Response to Employment Tribunal claim online form receipt'
+        mail.subject == t('response_email.subject', locale: locale)
       end
 
-      attr_accessor :mail
+      private
+
+      def has_correct_to_address_for?(input_data) # rubocop:disable Naming/PredicateName
+        mail.to.include?(input_data.email_receipt)
+      end
+
+      def assert_reference_element(reference)
+        assert_selector(:css, 'p', text: t('response_email.reference', locale: locale, reference: reference), wait: 0)
+      end
+
+      def assert_submission_date_element(submission_date)
+        assert_selector(:css, 'p', text: t('response_email.submission_date', locale: locale, submission_date: submission_date))
+      end
+
+      def has_submission_date_element?(submission_date)
+        assert_submission_date_element(submission_date)
+        true
+      rescue Capybara::ElementNotFound
+        false
+      end
+
+      def assert_submission_date
+        now = Time.zone.now
+
+        return if has_submission_date_element?(now.strftime('%d/%m/%Y'))
+        assert_submission_date_element((now - 1.minute).strftime('%d/%m/%Y'))
+      end
+
+      def assert_office_address_element(office_address)
+        assert_selector(:css,'p', text: t('response_email.office_address', locale: locale, address: office_address), wait: 0)
+      end
+
+      def assert_office_telephone_element(telephone)
+        assert_selector(:css, 'p', text: t('response_email.office_telephone', locale: locale, telephone: telephone), wait: 0)
+      end
+
+      def assert_office_name_element(office_name)
+        assert_selector(:css, 'p', text: t('response_email.office_name', locale: locale, office_name: office_name), wait: 0)
+      end
+
+      def attached_pdf_for(reference:)
+        mail.parts.attachments.detect { |a| a.filename == "#{reference}.pdf" }
+      end
+
+      attr_accessor :mail, :locale
     end
   end
 end
