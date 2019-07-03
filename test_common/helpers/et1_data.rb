8a8dbeb18cdb3130572be81031ea297cd47ec508
Sivvoy Taing <sivvo@yahoo.com>
2019-07-03 14:24:41 +0100

    return factory bot data in json

--- test_common/helpers/et1_data.rb
+++ test_common/helpers/et1_data.rb
@@ -1,26 +1,15 @@
-# def match_et1_data_for(expected)
-#   EtFullSystem::Test::Et1Data.new(expected)
-# end
-
-# module EtFullSystem
-#   module Test
-#     module Et1Data
-#       def initialize(claim:, claimants:, representative:, respondents:, employment:)
-#         self.claim = claim
-#         self.claimants = claimants
-#         self.representative = representative
-#         self.respondents = respondents
-#         self.employment = employment
-#       end
-
-#       def matches?(actual)
-#         self.page_object = EtFullSystem::Test::FileObjects::Et1CcdFile.new(actual)
-#         page_object.has_correct_contents_for?(claim: claim, claimants: claimants, respondents: respondents, representative: representative, employment: employment)
-#       end
-
-#       private
-
-#       attr_accessor :page_object, :claimants, :respondents, :representative, :employment, :claim
-#     end
-#   end
-# end
+module EtFullSystem
+  module Test
+    module Et1Data
+      include ::EtFullSystem::Test::I18n
+      def json_object(json)
+        hash = json[0].to_h
+        hash.map do |key, value|
+          value = t(value) if value.is_a?(Symbol)
+          hash[key.to_sym] = value
+        end
+        return hash
+      end
+    end
+  end
+end
