827a171da238c2c05f571d5c44e71238fca90702
Sivvoy Taing <sivvo@yahoo.com>
2019-07-04 16:07:00 +0100

    update ccd mapping

--- features/step_definitions/et1_ccd_steps.rb
+++ features/step_definitions/et1_ccd_steps.rb
@@ -1,12 +1,12 @@
 Then /^the claim should be present in CCD$/ do
   admin_api = EtFullSystem::Test::AdminApi.new atos_interface: atos_interface
   reference_number = admin_api.get_reference_number(claim_application_reference: @claim_application_reference)
-
-  #ccd_data = EtFullSystem::Test::CcdApi.new
+  sleep 10
   ccd_data = ccd.caseworker_search_latest_by_reference(reference_number, case_type_id: 'EmpTrib_MVP_1.0_Manc')
 
-  #response = ccd_data.caseworker_search_latest_by_reference(reference_number)
-  # expect(response).to include ccd_case_details(reference_number)
-
-  expect(ccd_data['case_fields']).to include ccd_claimant_details(@claimant).as_json
+  # ccd_data = EtFullSystem::Test::CcdApi.new
+  # response = ccd_data.caseworker_search_latest_by_reference(reference_number)
+  expect(ccd_data['case_fields']).to include ccd_case_details(reference_number)
+  expect(ccd_data['case_fields']).to include claimant_type(@claimant).as_json
+  expect(ccd_data['case_fields']).to include claimant_ind_type(@claimant).as_json
 end
--- test_common/helpers/et1_ccd_mapping.rb
+++ test_common/helpers/et1_ccd_mapping.rb
@@ -14,31 +14,99 @@ module EtFullSystem
       def ccd_case_details(data)
         {
           "receiptDate" => Time.now.strftime("%Y-%m-%d"),
-          "feeGroupReference" => data
+          "feeGroupReference" => data,
+          "caseType" => "Single"
         }
       end
 
-      def ccd_claimant_details(data)
+      def claimant_ind_type(data)
         et1_online_data = json_object(data)
-          {
-            "claimant_title1" => et1_online_data[:title],
-            "claimant_first_names" => et1_online_data[:first_name],
-            "claimant_last_name" => et1_online_data[:last_name],
-            "claimant_date_of_birth" => et1_online_data[:date_of_birth],
+        { 
+          "claimantIndtype" => {
             "claimant_gender" => et1_online_data[:gender],
-            "claimant_disabled" => et1_online_data[:has_special_needs],
-            "claimant_disabled_details" => et1_online_data[:special_needs],
-            "AddressLine1" => et1_online_data[:building],
-            "AddressLine2" => et1_online_data[:street],
-            "PostTown" => et1_online_data[:locality],
-            "County" => et1_online_data[:county],
-            "PostCode" => et1_online_data[:post_code],
-            "Country" => et1_online_data[:country],
-            "claimant_phone_number" => et1_online_data[:telephone_number],
-            "claimant_mobile_number" => et1_online_data[:alternative_telephone_number],
-            "claimant_email_address" => et1_online_data[:email_address],
-            "claimant_contact_preference" => et1_online_data[:correspondence]
+            "claimant_title1" => et1_online_data[:title],
+            "claimant_last_name" => et1_online_data[:last_name],
+            "claimant_first_names" => et1_online_data[:first_name],
+            "claimant_date_of_birth" => et1_online_data[:date_of_birth]
           }
+        }
+      end
+
+      def representativeClaimantType(data)
+        et1_online_data = json_object(data)
+        {
+          "representativeClaimantType" => {
+            "representative_dx" => "dx1234567890",
+            "name_of_organisation" => "generate dynamic supply-chains",
+            "name_of_representative" => "Alphonso Walter",
+            "representative_address" => {
+              "County" => "Greater London",
+              "PostCode" => "SW1H 9PP",
+              "PostTown" => "London",
+              "AddressLine1" => "106",
+              "AddressLine2" => "Mayfair"
+            },
+            "representative_occupation" => "Solicitor",
+            "representative_phone_number" => "01111 123456",
+            "representative_email_address" => "sivvoy.taing@hmcts.net",
+            "representative_mobile_number" => "02222 654321"
+          }
+        }
+      end
+
+      def claimantWorkAddress(data)
+        et1_online_data = json_object(data)
+        {
+          "claimantWorkAddress" => {
+            "claimant_work_address" => {
+              "County" => "Greater London",
+              "PostCode" => "WD18 7SQ",
+              "PostTown" => "London",
+              "AddressLine1" => "101",
+              "AddressLine2" => "Petty France"
+            },
+            "claimant_work_phone_number"=>"03333 423554"
+          }
+        }
+      end
+
+      def respondent_sum_type(data)
+        et1_online_data = json_object(data)
+        {
+          "respondentSumType" => {
+            "respondent_ACAS" => "AC123456/78/90",
+            "respondent_name" => "Douglas, Hand and Breitenberg",
+            "respondent_phone1" => "02222 321654",
+            "respondent_address" => {
+              "County" => "Greater London",
+              "PostCode" => "SW1H 9QR",
+              "PostTown" => "London",
+              "AddressLine1" => "1681",
+              "AddressLine2" => "Guy Village"
+            },
+            "respondent_ACAS_question"=>"Yes"
+          }
+        }
+      end
+
+      def claimant_type(data)
+        et1_online_data = json_object(data)
+        {
+          "claimantType" => {
+          "claimant_addressUK" => {
+            "County" => et1_online_data[:county],
+             "Country" => et1_online_data[:country],
+             "PostCode" => et1_online_data[:post_code],
+             "PostTown" => et1_online_data[:locality],
+             "AddressLine1" => et1_online_data[:building],
+             "AddressLine2" => et1_online_data[:street]
+            },
+          "claimant_phone_number" => et1_online_data[:telephone_number],
+          "claimant_mobile_number" => et1_online_data[:alternative_telephone_number],
+          "claimant_email_address" => et1_online_data[:email_address],
+          "claimant_contact_preference" => et1_online_data[:correspondence]
+          }
+        }
       end
     end
   end
