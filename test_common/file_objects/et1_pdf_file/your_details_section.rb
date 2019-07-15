5f88d2d7f52fb83fe8346539f12ae2f80a275448
Sivvoy Taing <sivvo@yahoo.com>
2019-07-15 18:15:26 +0100

    fix pdf test when gender = prefer_not_to_say

--- test_common/file_objects/et1_pdf_file/your_details_section.rb
+++ test_common/file_objects/et1_pdf_file/your_details_section.rb
@@ -5,7 +5,26 @@ module EtFullSystem
       module Et1PdfFileSection
         class YourDetailsSection < EtFullSystem::Test::FileObjects::Et1PdfFileSection::Base
           def has_contents_for?(claimant:)
-            expected_values = {
+            if claimant.gender == :"simple_form.options.claimant.gender.prefer_not_to_say"
+              expected_values = {
+                title: title_for(claimant.title).camelcase,
+                first_name: claimant.first_name,
+                last_name: claimant.last_name,
+                dob_day: claimant.date_of_birth.split('/')[0],
+                dob_month: claimant.date_of_birth.split('/')[1],
+                dob_year: claimant.date_of_birth.split('/')[2],
+                gender: nil,
+                building: claimant.building,
+                street: claimant.street,
+                locality: claimant.locality,
+                county: claimant.county,
+                post_code: post_code_for(claimant.post_code),
+                telephone_number: claimant.telephone_number,
+                alternative_telephone_number: claimant.alternative_telephone_number,
+                email_address: claimant.email_address,
+                correspondence: contact_preference_for(claimant.correspondence)
+              }
+            else expected_values = {
                 title: title_for(claimant.title).camelcase,
                 first_name: claimant.first_name,
                 last_name: claimant.last_name,
@@ -23,9 +42,9 @@ module EtFullSystem
                 email_address: claimant.email_address,
                 correspondence: contact_preference_for(claimant.correspondence)
             }
+            end
             expect(mapped_field_values).to include expected_values
           end
-
         end
       end
     end
