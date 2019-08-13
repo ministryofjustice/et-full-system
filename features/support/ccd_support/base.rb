8567507d68013440f8344c47aa663fb1990cfec5
Sivvoy Taing <sivvo@yahoo.com>
2019-08-13 13:06:41 +0100

    remove hard coded username and password

--- features/support/ccd_support/base.rb
+++ features/support/ccd_support/base.rb
@@ -6,7 +6,7 @@ module EtFullSystem
         def self.ccd
           return @ccd if defined?(@ccd)
           @ccd = ::EtCcdClient::UiClient.new
-          @ccd.login(username: 'm@m.com', password: 'Pa55word11')
+          @ccd.login(::EtFullSystem::Test::Configuration.ccd_sidam_username, ::EtFullSystem::Test::Configuration.ccd_sidam_password)
           @ccd
         end
 
