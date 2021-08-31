e22d74cc880afd570ad44307dc8047342e4bccb7
Gary Taylor <gary.taylor@hismessages.com>
2021-08-31 18:06:23 +0100

    RST-3058 All tests now passing apart from 3 which are application failures

--- test_common/page_objects/et1/submission_page.rb
+++ test_common/page_objects/et1/submission_page.rb
@@ -8,16 +8,16 @@ module EtFullSystem
           element :page_header, :main_header, 'claim_reviews.show.header', exact: false
         end
         include EtFullSystem::Test::I18n
-        section :group_claim_section, SubmissionPageSections::GroupClaimSection, :et1_review_section_labelled, 'review.group_claim.title'
-        section :additional_respondents_section, SubmissionPageSections::AdditionalRespondentsSection, :et1_review_section_labelled, 'review.additional_respondents.title'
-        section :additional_information_section, SubmissionPageSections::AdditionalInformationSection, :et1_review_section_labelled, 'review.additional_information.title'
-        section :claim_details_section, SubmissionPageSections::ClaimDetailsSection,:et1_review_section_labelled, 'review.claim_details.title'
-        section :claim_outcome_section, SubmissionPageSections::ClaimOutcomeSection,  :et1_review_section_labelled, 'review.claim_outcome.title'
-        section :claim_type_section, SubmissionPageSections::ClaimTypeSection, :et1_review_section_labelled, 'review.claim_type.title'
-        section :claimant_section, SubmissionPageSections::ClaimantSection,:et1_review_section_labelled, 'review.claimant.title'
-        section :employment_section, SubmissionPageSections::EmploymentSection ,:et1_review_section_labelled, 'review.employment.title'
-        section :representative_section, SubmissionPageSections::RepresentativeSection,:et1_review_section_labelled, 'review.representative.title'
-        section :respondent_section, SubmissionPageSections::RespondentSection,:et1_review_section_labelled, 'review.respondent.title'
+        section :group_claim_section, SubmissionPageSections::GroupClaimSection, :govuk_summary_list, :'review.group_claim.title'
+        section :additional_respondents_section, SubmissionPageSections::AdditionalRespondentsSection, :govuk_summary_list, :'review.additional_respondents.title'
+        section :additional_information_section, SubmissionPageSections::AdditionalInformationSection, :govuk_summary_list, :'review.additional_information.title'
+        section :claim_details_section, SubmissionPageSections::ClaimDetailsSection,:govuk_summary_list, :'review.claim_details.title'
+        section :claim_outcome_section, SubmissionPageSections::ClaimOutcomeSection,  :govuk_summary_list, :'review.claim_outcome.title'
+        section :claim_type_section, SubmissionPageSections::ClaimTypeSection, :govuk_summary_list, :'review.claim_type.title'
+        section :claimant_section, SubmissionPageSections::ClaimantSection,:govuk_summary_list, :'review.claimant.title'
+        section :employment_section, SubmissionPageSections::EmploymentSection ,:govuk_summary_list, :'review.employment.title'
+        section :representative_section, SubmissionPageSections::RepresentativeSection,:govuk_summary_list, :'review.representative.title'
+        section :respondent_section, SubmissionPageSections::RespondentSection,:govuk_summary_list, :'review.respondent.title'
 
         gds_submit_button :submit_claim_button, :'review.submit_claim'
 
