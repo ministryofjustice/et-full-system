14ae028bbf96d168abf4c3aa61e7f4bb6bdf9004
Gary Taylor <gary.taylor@hismessages.com>
2021-10-12 07:52:53 +0100

    Changes made for RST-2055 (diversity edit links)

--- test_common/page_objects/diversity/submission_form_page.rb
+++ test_common/page_objects/diversity/submission_form_page.rb
@@ -16,69 +16,59 @@ module EtFullSystem
           include EtTestHelpers::Section
           section :summary, :govuk_summary_list do
             #What is the type of your claim?
-            section :claim_type_row, :govuk_summary_list_row, :'summary.claim' do
+            section :claim_type_row, :govuk_summary_list_row, :'summary.claim.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.claim.edit_link'
             end
             #What is your sex?
-            section :sex_row, :govuk_summary_list_row, :'summary.sex' do
+            section :sex_row, :govuk_summary_list_row, :'summary.sex.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
-            end
-            #What is your gender identity?
-            section :gender_row, :govuk_summary_list_row, :'summary.gender' do
-              element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
-            end
-            #Is your gender identity different to the sex you were assumed to be at birth?
-            section :gender_at_birth_row, :govuk_summary_list_row, :'summary.gender_at_birth' do
-              element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.sex.edit_link'
             end
             #Which of the options below best describes your sexual identity?
-            section :sexual_identity_row, :govuk_summary_list_row, :'summary.sexual_identity' do
+            section :sexual_identity_row, :govuk_summary_list_row, :'summary.sexual_identity.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.sexual_identity.edit_link'
             end
             #What is your relationship status?
-            section :relationship_row, :govuk_summary_list_row, :'summary.relationship' do
+            section :relationship_row, :govuk_summary_list_row, :'summary.relationship.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.relationship.edit_link'
             end
             #Which age group are you in?
-            section :age_row, :govuk_summary_list_row, :'summary.age' do
+            section :age_row, :govuk_summary_list_row, :'summary.age.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.age.edit_link'
             end
             #Do you have any caring responsibilities?
-            section :care_row, :govuk_summary_list_row, :'summary.care' do
+            section :care_row, :govuk_summary_list_row, :'summary.care.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.care.edit_link'
             end
             #What is your religion?
-            section :religion_row, :govuk_summary_list_row, :'summary.religion' do
+            section :religion_row, :govuk_summary_list_row, :'summary.religion.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.religion.label'
             end
             #What is your ethnic group?
-            section :ethnicity_group_row, :govuk_summary_list_row, :'summary.ethnicity_group' do
+            section :ethnicity_group_row, :govuk_summary_list_row, :'summary.ethnicity_group.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.ethnicity_group.edit_link'
             end
             #What is your ethnic type?
-            section :ethnicity_type_row, :govuk_summary_list_row, :'summary.ethnicity_type' do
+            section :ethnicity_type_row, :govuk_summary_list_row, :'summary.ethnicity_type.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.ethnicity_type.edit_link'
             end
             #Do you have any physical or mental health conditions or illnesses lasting or expected to last for 12 months or more?
-            section :health_row, :govuk_summary_list_row, :'summary.health' do
+            section :health_row, :govuk_summary_list_row, :'summary.health.label' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.health.edit_link'
             end
             #Were you pregnant when you were dismissed?
             section :pregnancy_row, :govuk_summary_list_row, :'summary.pregnancy' do
               element :answer, :govuk_summary_list_col
-              element :link, :link_named, 'diversities.edit'
+              element :link, :link_named, 'summary.pregnancy.edit_link'
             end
           end
 
