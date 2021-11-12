1de0da7b8a3dcee802244fbf677f949c37c8abdc
Gary Taylor <gary.taylor@hismessages.com>
2021-11-12 11:24:16 +0000

    RST-3058 Getting ready to merge to develop

--- features/support/capybara_selectors/link_named.rb
+++ features/support/capybara_selectors/link_named.rb
@@ -3,6 +3,6 @@ Capybara.add_selector(:link_named) do
   xpath do |locator, _options|
     translated = EtFullSystem::Test::Messaging.instance.translate(locator)
     @definition.label("Link named <#{translated}> (#{locator}) ")
-    XPath.generate { |x| x.descendant(:a)[x.string.n.is(translated)] }
+    XPath.generate { |x| x.descendant(:a)[x.string.n.starts_with(translated)] }
   end
 end
